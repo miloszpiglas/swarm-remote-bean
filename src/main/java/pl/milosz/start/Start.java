0d403a961cb211945e593d9c55262cf92410cf6f
Miłosz Pigłas <milosz@archeocs.com>
2015-12-08 10:40:37 +0100

    remote bean with infinispan

---
+++ src/main/java/pl/milosz/start/Start.java
@@ -0,0 +1,48 @@
+package pl.milosz.start;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.wildfly.swarm.config.infinispan.CacheContainer;
+import org.wildfly.swarm.config.infinispan.cache_container.*;
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.container.JARArchive;
+import org.wildfly.swarm.infinispan.InfinispanFraction;
+//import org.wildfly.swarm.jgroups.JGroupsFraction;
+
+import pl.milosz.bean.Greeter;
+import pl.milosz.bean.IGreeter;
+
+public class Start {
+
+	public static void main(String[] args) throws Exception {
+		Container container = new Container();
+		//
+		// ManagementFraction mngm = ManagementFraction.createDefaultFraction()
+		// .httpInterfaceManagementInterface((iface) -> {
+		// iface.securityRealm("ManagementRealm");
+		// }).securityRealm("ManagementRealm", (realm) -> {
+		// realm.inMemoryAuthentication((authn) -> {
+		// authn.add("appuser", "appuser", true);
+		// });
+		// realm.inMemoryAuthorization((authz) -> {
+		// authz.add("user", "admin");
+		// });
+		// }).securityRealm("ApplicationRealm", (realm) -> {
+		// realm.inMemoryAuthentication((authn) -> {
+		// authn.add("appuser", "appuser", true);
+		// });
+		// realm.inMemoryAuthorization((authz) -> {
+		// authz.add("appuser", "admin");
+		// });
+		// });
+		// container.fraction(mngm);
+		// container.fraction(MessagingFraction.createDefaultFraction());
+
+		
+		container.fraction(InfinispanFraction.createDefaultFraction());
+		container.start();
+		JARArchive archive = ShrinkWrap.create(JARArchive.class, "SwarmRemote");
+		archive.addClasses(IGreeter.class, Greeter.class);
+		container.deploy(archive);
+		System.out.println("OK");
+	}
+}
